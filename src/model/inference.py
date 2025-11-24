369ebc627090a98cf5f67dcc7a3ab92d50d0a267
Mark <mark.mueller40@gmx.de>
2025-11-24 10:24:04 +0100

    add recurrent logic into inference system

--- src/model/inference.py
+++ src/model/inference.py
@@ -7,38 +7,55 @@ from src.model.layers import (
 
 @dataclass(frozen=True)
 class InferenceContext:
-    fmap_2d: np.ndarray
-    fmap_flat: np.ndarray
-    pooled_idx: np.ndarray
+    fmap_2d: list[np.ndarray]
+    fmap_flat: list[np.ndarray]
+    pooled_idx: list[np.ndarray]
     spec: StackSpec
     slices: any  # BlockSlices
 
 def prepare_context(model, x_img) -> InferenceContext:
-    fmap_2d = conv2d_valid_stride(x_img, model.kernel_weights, model.stride)
-    fmap_flat = fmap_2d.ravel()
+    fmap_2d = []
+    fmap_flat = []
+    conv_active = []
+    seq = []
+    pooled_idexes = []
+    num_pooled_units_per_recurrent_layer = []
 
-    pooled_idx = pooled_indices_for_input(
-        fmap_flat=fmap_flat,
-        num_conv_units=model.num_conv_units,
-        pooling_type=model.pooling_type,
-        pool_windows=getattr(model, "pool_windows", []),
-    )
+    for recurrent_layer in range(model.num_recurrent_layers):
+        currentlayer_fmap_2d = conv2d_valid_stride(x_img, model.kernel_weights, model.stride)
+        currentlayer_fmap_flat = currentlayer_fmap_2d.ravel()
+        fmap_2d.append(currentlayer_fmap_2d)
+        fmap_flat.append(currentlayer_fmap_flat)
 
-    conv_active = len(pooled_idx) if model.pooling_type == "deterministic" else model.num_conv_units
+        pooled_idx = pooled_indices_for_input(
+            fmap_flat=currentlayer_fmap_flat,
+            num_conv_units=model.num_conv_units,
+            pooling_type=model.pooling_type,
+            pool_windows=getattr(model, "pool_windows", []),
+        )
+
+        conv_active.append(len(pooled_idx) if model.pooling_type == "deterministic" else model.num_conv_units)
+        seq.append(SeqSpec(model.sequential_layer_sizes))
+        pooled_idexes.append(pooled_idx)
+        num_pooled_units_per_recurrent_layer.append(len(pooled_idx))
+
     spec = StackSpec(
         conv_active=conv_active,
-        seq=SeqSpec(tuple(model.sequential_layer_sizes)),
-        n_out=int(model.num_lable_nodes),
+        seq=seq,
+        n_out=model.num_lable_nodes,
         pooling_type=model.pooling_type,
-        n_pooled_units=len(pooled_idx)
+        n_pooled_units=num_pooled_units_per_recurrent_layer,
+        num_recurrent_layers=model.num_recurrent_layers
     )
 
     slices = build_slices(spec)
 
+    # TODO: do not build spec and slices every time
+
     return InferenceContext(
         fmap_2d=fmap_2d,
         fmap_flat=fmap_flat,
-        pooled_idx=pooled_idx,
+        pooled_idx=pooled_idexes,
         spec=spec,
         slices=slices,
     )

e6fd14baa669b1f1959cc7c07006d2671e5a8511
Mark <mark.mueller40@gmx.de>
2025-12-29 13:08:27 +0100

    remove intralayer weights as hyperparameter for now

--- slurm_hyperparam.py
+++ slurm_hyperparam.py
@@ -352,7 +352,7 @@ if __name__ == '__main__':
                         default=1000,
                         type=int)
 
-    parser.add_argument('--sweep_id', type=str, default="qd0erlra") #v8vwy5dq breast mnistk3i5g39d current rbm penumonia estex4pi   current sq qbm xzvm3exu
+    parser.add_argument('--sweep_id', type=str, default="ccdy9yut") #v8vwy5dq breast mnistk3i5g39d current rbm penumonia estex4pi   current sq qbm xzvm3exu
     parser.add_argument('--key', type=str, default=None)
 
     args = parser.parse_args()

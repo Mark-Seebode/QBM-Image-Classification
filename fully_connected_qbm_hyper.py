916cd581e87ad03d0dfebecabf3d79be60fcbc68
Mark <mark.mueller40@gmx.de>
2026-01-17 12:21:28 +0100

    extend lr maximum to 0.5

--- fully_connected_qbm_hyper.py
+++ fully_connected_qbm_hyper.py
@@ -328,7 +328,7 @@ if __name__ == '__main__':
                         default=1000,
                         type=int)
 
-    parser.add_argument('--sweep_id', type=str, default="8q200wls") #v8vwy5dq breast mnistk3i5g39d current rbm penumonia estex4pi   current sq qbm xzvm3exu
+    parser.add_argument('--sweep_id', type=str, default="tvedakzy") #2vifgbnq 8q200wls mnistk3i5g39d current rbm penumonia estex4pi   current sq qbm xzvm3exu
     parser.add_argument('--key', type=str, default=None)
 
     args = parser.parse_args()
